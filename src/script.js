8f2263930a462a5579b764754ed10d3a952b1dc9
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2025-06-07 15:56:28 +0200

    Searchable and clickable
    the searchbar displays the results of the search and once an element is clicked the seach is cleared

--- src/script.js
+++ src/script.js
@@ -1,17 +1,40 @@
+let data;
+
 function searchCharacter() {
+
     let search = document.getElementById("search-field").value.trim().toLowerCase();
-    console.log(search);
+    document.getElementById("search-results").innerHTML = "";
+
     if (search.length >= 2) {
         axios({method: 'GET', url: "https://swapi.info/api/people"}).then((response) => {
+            data = response.data;
+
+            console.log(data);
             for (let i = 0; i < response.data.length; i++) {
                 if (response.data[i].name.toLowerCase().includes(search)) {
-                    console.log(response.data[i]);
+                    const dropdown = document.getElementById("search-results");
+                    const li = document.createElement("li");
+                    li.textContent = response.data[i].name;
+                    li.addEventListener("click", addCharacter)
+                    dropdown.appendChild(li);
                 }
             }
 
         })
     }
 
+}
+
+function addCharacter(e) {
+    let chosenCharacter;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].name.toLowerCase() == e.target.textContent.toLowerCase()) {
+            chosenCharacter = data[i];
+            break;}
+    }
+    document.getElementById("search-results").innerHTML = "";
+    document.getElementById("search-field").value = "";
+    console.log(chosenCharacter.name + "FOUND");
 }
 
 document.getElementById('search-field').addEventListener('keyup', searchCharacter);
